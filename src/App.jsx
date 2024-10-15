71ac8d32d406315d127a7a1155cbd36fedf1970a
Joshua Yustana <joshiosimoe@gmail.com>
2024-10-15 16:28:23 -0500

    Spinning Cubes

--- src/App.jsx
+++ src/App.jsx
@@ -80,12 +80,12 @@ const App = () => {
         <directionalLight position={[0,0,2]}/> intensity={1}
         {/* <ambientLight intensity={1}/> */}
 
-        {/* <group position={[0,-1,0]}>
+        <group position={[0,-1,0]}>
           <Cube position={[1,0,0]} color="green" size={[1,1,1]}/>
           <Cube position={[-1,0,0]} color="yellow" size={[1,1,1]}/>
           <Cube position={[1,2,0]} color="red" size={[1,1,1]}/>
           <Cube position={[-1,2,0]} color="blue" size={[1,1,1]}/>
-        </group> */}
+        </group>
 
         <Sphere position={[0,0,0]} size={[1,30,30]} color={"orange"}/>
         {/* <Torus position={[2,0,0]} size={[0.8, 0.1, 30, 30]} color={"blue"}/>
